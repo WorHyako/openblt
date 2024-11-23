09e174228c7a0c3d9b07fba310d8673cadc356fc
WorHyako <worhyako@gmail.com>
2024-11-23 14:19:28 +0100

    replace defines from compiler flags to definition block

--- Host/Source/LibOpenBLT/cmake/CompilerFlags.cmake
+++ Host/Source/LibOpenBLT/cmake/CompilerFlags.cmake
@@ -1,12 +1,5 @@
 cmake_minimum_required(VERSION 3.15)
 
-# Set platform specific compiler macros PLATFORM_XXX
-if (CMAKE_SIZEOF_VOID_P EQUAL 4)
-    set(PointerSizeFlag "-DPLATFORM_32BIT")
-else ()
-    set(PointerSizeFlag "-DPLATFORM_64BIT")
-endif ()
-
 if (CMAKE_C_COMPILER_ID MATCHES GNU)
     set(CompilerFlag "-std=gnu99")
 elseif (CMAKE_C_COMPILER_ID MATCHES MSVC)
@@ -16,9 +9,9 @@ elseif (CMAKE_C_COMPILER_ID MATCHES MSVC)
 endif ()
 
 if(WIN32)
-    set(PlatformFlag "-DPLATFORM_WINDOWS -D_CRT_SECURE_NO_WARNINGS")
+    set(PlatformFlag "-D_CRT_SECURE_NO_WARNINGS")
 elseif (UNIX)
-    set(PlatformFlag "-DPLATFORM_LINUX -pthread")
+    set(PlatformFlag "-pthread")
 endif ()
 
 list(APPEND CMAKE_C_FLAGS ${PlatformFlag} ${PointerSizeFlag} ${CompilerFlag})
